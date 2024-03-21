03c5e086e97481ab97de0ef93b5ec0d0fc53011a
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-03-21 20:37:19 +0100

    pre-commit run -a

--- src/graph-widget.cc
+++ src/graph-widget.cc
@@ -124,12 +124,8 @@ GraphWidget::GraphWidget(QString name, QWidget *parent)
   QHBoxLayout *hLayout = new QHBoxLayout(buttonBox_);
   hLayout->setMargin(0);
   algList_ = new QComboBox(this);
-  algList_->addItems(QStringList() << "dot"
-                                   << "neato"
-                                   << "fdp"
-                                   << "sfdp"
-                                   << "twopi"
-                                   << "circo");
+  algList_->addItems(QStringList() << "dot" << "neato" << "fdp" << "sfdp"
+                                   << "twopi" << "circo");
   // << "patchwork" << "osage");
   QPushButton *saveas = new QPushButton(QIcon::fromTheme("document-save-as"),
                                         "&Save DOT file...", buttonBox_);
