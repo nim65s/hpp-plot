f5299941a79cc342d37b0bfa34c935903922f91f
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-04-16 12:27:53 +0200

    sync with gepetto/nix template

--- flake.nix
+++ flake.nix
@@ -1,39 +1,29 @@
 {
   description = "Graphical utilities for constraint graphs in hpp-manipulation";
 
-  inputs = {
-    gepetto.url = "github:gepetto/nix";
-    flake-parts.follows = "gepetto/flake-parts";
-    systems.follows = "gepetto/systems";
-  };
+  inputs.gepetto.url = "github:gepetto/nix";
 
   outputs =
     inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } (
+    inputs.gepetto.lib.mkFlakoboros inputs (
       { lib, ... }:
       {
-        systems = import inputs.systems;
-        imports = [
-          inputs.gepetto.flakeModule
-          {
-            flakoboros.overrideAttrs.hpp-plot = _: {
-              patches = [ ];
-              src = lib.fileset.toSource {
-                root = ./.;
-                fileset = lib.fileset.unions [
-                  ./bin
-                  ./cmake_modules
-                  ./CMakeLists.txt
-                  ./doc
-                  ./include
-                  ./package.xml
-                  ./plugins
-                  ./src
-                ];
-              };
-            };
-          }
-        ];
+        overrideAttrs.hpp-plot = {
+          patches = [ ];
+          src = lib.fileset.toSource {
+            root = ./.;
+            fileset = lib.fileset.unions [
+              ./bin
+              ./cmake_modules
+              ./CMakeLists.txt
+              ./doc
+              ./include
+              ./package.xml
+              ./plugins
+              ./src
+            ];
+          };
+        };
       }
     );
 }
